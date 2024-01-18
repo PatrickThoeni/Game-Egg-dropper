918e0907737a04e9db20aa460684f7def8b4872c
PatrickThoeni <157055360+PatrickThoeni@users.noreply.github.com>
2024-01-18 20:10:03 +0100

    Update egg_catcher.py
    - change Background (local png file)
    - adding bombs which drop just like the eggs
    - decrease the score if bombs are catched

--- egg_catcher.py
+++ egg_catcher.py
@@ -1,24 +1,41 @@
 from itertools import cycle
 from random import randrange
-from tkinter import Canvas, Tk, messagebox, font
+import sys
+import pygame
+from tkinter import Canvas, Label, PhotoImage, Tk, font, messagebox, ttk
+
+pygame.init()
+
+#um Bild als Hintergrund zu nehmen:
 
 canvas_width = 800
 canvas_height = 400
 
 root = Tk()
 root.title("Egg Catcher")
-c = Canvas(root, width=canvas_width, height=canvas_height, background="deep sky blue")
-c.create_rectangle(-5, canvas_height-100, canvas_width+5, canvas_height+5, fill="sea green", width=0)
-c.create_oval(-80, -80, 120, 120, fill='orange', width=0)
-c.pack()
 
-color_cycle = cycle(["light blue", "light green", "light pink", "light yellow", "light cyan"])
+bg = PhotoImage(file="C:\\Users\\patri\\OneDrive\\Dokumente\\HTL\\Schuljahr 2023-24\\WDIC\\Python_project\\Bilder\\pic2.png")
+
+c = Canvas(root, width=canvas_width, height=canvas_height)
+
+c.pack(fill="both", expand=True)
+c.create_image(0,0, image = bg, anchor="nw")
+
+color_cycle_egg = cycle(["light green", "light pink", "light yellow", "light cyan"])
 egg_width = 45
 egg_height = 55
-egg_score = 10
-egg_speed = 500
-egg_interval = 4000
-difficulty = 0.95
+egg_score = 1
+egg_speed = 15
+egg_interval = randrange(3000, 5000)
+
+difficulty = 1
+
+color_cycle_bomb = cycle(["black", "red"])
+bomb_width = 30
+bomb_height = bomb_width
+bomb_speed = 15
+bomb_interval = randrange(2500, 5000)
+
 catcher_color = "blue"
 catcher_width = 100
 catcher_height = 100
@@ -39,21 +56,40 @@ lives_remaining = 3
 lives_text = c.create_text(canvas_width-10, 10, anchor="ne", font=game_font, fill="darkblue", text="Lives: "+ str(lives_remaining))
 
 eggs = []
+bombs = []
 
 def create_egg():
     x = randrange(10, 740)
     y = 40
-    new_egg = c.create_oval(x, y, x+egg_width, y+egg_height, fill=next(color_cycle), width=0)
+    
+    new_egg = c.create_oval(x, y, x+egg_width, y+egg_height, fill = next(color_cycle_egg), width=0)
     eggs.append(new_egg)
     root.after(egg_interval, create_egg)
+
+def create_bomb():
+    x = randrange(10, 740)
+    y=40
+    new_bomb = c.create_oval(x, y, x+bomb_width, y+bomb_height, fill=next(color_cycle_bomb), width = 0)
+    bombs.append(new_bomb)
+    root.after(bomb_interval, create_bomb)
+
 
 def move_eggs():
     for egg in eggs:
         (eggx, eggy, eggx2, eggy2) = c.coords(egg)
-        c.move(egg, 0, 10)
+        c.move(egg, 0, 1)
         if eggy2 > canvas_height:
             egg_dropped(egg)
     root.after(egg_speed, move_eggs)
+
+def move_bombs():
+    for bomb in bombs:
+        (bombx, bomby, bombx2, bomby2) = c.coords(bomb)
+        c.move(bomb, 0, 1)
+        if bomby2 > canvas_height:
+            bomb_dropped(bomb)
+    root.after(bomb_speed, move_bombs)
+
 
 def egg_dropped(egg):
     eggs.remove(egg)
@@ -63,12 +99,17 @@ def egg_dropped(egg):
         messagebox.showinfo("Game Over!", "Final Score: "+ str(score))
         root.destroy()
 
+def bomb_dropped(bomb):
+    bombs.remove(bomb)
+    c.delete(bomb)
+
+
 def lose_a_life():
     global lives_remaining
     lives_remaining -= 1
     c.itemconfigure(lives_text, text="Lives: "+ str(lives_remaining))
 
-def check_catch():
+def check_catch_egg():
     (catcherx, catchery, catcherx2, catchery2) = c.coords(catcher)
     for egg in eggs:
         (eggx, eggy, eggx2, eggy2) = c.coords(egg)
@@ -76,14 +117,34 @@ def check_catch():
             eggs.remove(egg)
             c.delete(egg)
             increase_score(egg_score)
-    root.after(100, check_catch)
+    root.after(100, check_catch_egg)
+
+def check_catch_bomb():
+    (catcherx, catchery, catcherx2, catchery2) = c.coords(catcher)
+    for bomb in bombs:
+        (bombx, bomby, bombx2, bomby2) = c.coords(bomb)
+        if catcherx < bombx and bombx2 < catcherx2 and catchery2 - bomby2 < 40:
+            bombs.remove(bomb)
+            c.delete(bomb)
+            decrease_score(egg_score)
+    root.after(100,check_catch_bomb)
 
 def increase_score(points):
-    global score, egg_speed, egg_interval
+    global score, egg_speed, egg_interval, bomb_speed, bomb_interval
     score += points
     egg_speed = int(egg_speed * difficulty)
     egg_interval = int(egg_interval * difficulty)
+
     c.itemconfigure(score_text, text="Score: "+ str(score))
+
+def decrease_score(points):
+    global score, egg_speed, egg_interval, bomb_speed, bomb_interval
+    score-=points
+    bomb_speed = int(bomb_speed * difficulty)
+    bomb_interval = int(bomb_interval * difficulty)
+
+    c. itemconfigure(score_text, text="Score: "+ str(score))
+    
 
 def move_left(event):
     (x1, y1, x2, y2) = c.coords(catcher)
@@ -97,10 +158,14 @@ def move_right(event):
 
 c.bind("<Left>", move_left)
 c.bind("<Right>", move_right)
+
+
 c.focus_set()
 root.after(1000, create_egg)
 root.after(1000, move_eggs)
-root.after(1000, check_catch)
-root.mainloop()
+root.after(1000, check_catch_egg)
 
-#Coded with 💙 by Mr. Unity Buddy
+root.after(1000, create_bomb)
+root.after(1000, move_bombs)
+root.after(1000, check_catch_bomb)
+root.mainloop()
